e294974f517fa68bcf0e05aa87a22abf21e01a38
spaghettifying <spaghettifying@users.noreply.github.com>
2023-02-08 02:54:05 +0000

    Added class diagrams and input validation
    Added class diagrams using Class Diagramer in Visual Studio 22
    Added input validation for the InputDelayField to help for testing for write up

--- Assets/InputDelayScript.cs
+++ Assets/InputDelayScript.cs
@@ -1,3 +1,4 @@
+using System;
 using Assets;
 using System.Collections;
 using System.Collections.Generic;
@@ -7,9 +8,29 @@ using UnityEngine.UI;
 
 public class InputDelayScript : MonoBehaviour
 {
+    private InputField inputField;
+
+    private void Start()
+    {
+        inputField = GameObject.Find("InputDelayField").GetComponent<InputField>();
+    }
+
     public void input(InputField _input)
     {
-        Simulation.stepDelay = float.Parse(_input.text);
+        try
+        {
+            Simulation.stepDelay = float.Parse(_input.text);
+            inputField.placeholder.GetComponent<Text>().text = "Enter delay (s)";
+            inputField.placeholder.GetComponent<Text>().color = Color.black;
+        }
+        catch (FormatException e)
+        {
+            inputField.text = "";
+            inputField.placeholder.GetComponent<Text>().text = "Invalid Input";
+            inputField.placeholder.GetComponent<Text>().color = Color.red;
+            Debug.Log("User entered non-float value into Delay input field, " + e);
+            throw;
+        }
         Debug.Log($"Step Delay set to: {Simulation.stepDelay}");
     }
 }
